8352c618b7eb458d6dcb6ae9beeb84459117548d
Harshita Dubey <71823970+Harshitadubey11@users.noreply.github.com>
2022-08-27 15:06:39 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+#---------------------------SUPERMARKET MANAGEMENT SYSTEM------------------------------
+thisdict={
+	
+	"brand":"Ford",
+	"model":"Mustang",
+	"year":1964
+
+	}
+
+thisdict["year"]=2018
+print(thisdict)
